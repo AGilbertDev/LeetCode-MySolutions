ea1269c4e66bb1db151a0563a85332529176816c
Alexandre Gilbert <78440373+AGilbertDev@users.noreply.github.com>
2022-04-02 15:24:53 -0400

    Time: 3 ms (51.76%), Space: 41.9 MB (60.18%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
     public static boolean isValid(String s) {
 
-        /** Each opening parentese must be added to a stack (Last in first out). It is removed from the top only when its             closing bracket is met. If a closing bracket doesn't match the last one on the stack, then the sequence is               not valid. **/
+        /* Each opening parenthese must be added to a stack (Last in first out). It is removed from the top only when                its closing one is met. If a closing bracket doesn't match the last one on the stack, then the sequence is                not valid. */
         
         if (s.length() % 2 != 0) { // Eliminates string of odd length immediately.
             return false;
         }
 
-        Stack<Character> stack = new Stack<>(); // This will store the order of opening parenteses.
+        Stack<Character> stack = new Stack<>(); // This will store the order of opening parentheses.
 
         for (int i = 0; i < s.length(); i++) { // Reads each character from the string.
-            char c = s.charAt(i); // Current character.
+            char c = s.charAt(i);              // Current character.
 
-            if (c == '(' || c == '[' || c == '{') { // Looks if it's an opening parentese and adds it on the stack.
+            if (c == '(' || c == '[' || c == '{') { // Looks if it's an opening parenthese and adds it on the stack.
                 stack.push(c);
             }
 
-            else { // If it's a closing parentese, looks for the last item on the stack.
+            else { // If it's a closing parenthese, looks for the last item on the stack.
                 
                 if (stack.size() == 0) { // hecks if the opening bracket was met.
                     return false;
                 }
 
-                switch (c) { // If the last opening parentese matches the one that closes, removes it from the top of the                              // stack. And if not, the order is incorrect and the program stops.
+                switch (c) { /* If the last opening parenthese matches the one that closes, removes it from the top of                                    the stack. And if not, the order is incorrect and the program stops. */
                     case ')':
                         if (stack.lastElement() == '(') {
                             stack.pop();
@@ -48,7 +48,7 @@ class Solution {
             }
         }
         
-        if (stack.size() != 0) { // Looks for unclosed parenteses.
+        if (stack.size() != 0) { // Looks for unclosed parentheses.
             return false;
         }
 
